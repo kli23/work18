0d6162fc8d228038c27c673ccd4382dfcaf48678
Kevin Li <kevinlouisli7@gmail.com>
2021-12-08 11:51:23 -0500

    Created makefile, skeleton of plumbing.c code

---
+++ plumbing.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define READ 0
+#define WRITE 1
+
+
+int main() {
+    int fds[2];
+    int fds2[2];
+    pipe( fds );
+    pipe( fds2 );
+    char line[100];
+
+    f = fork();
+    if (f == 0) {
+
+    }
+    else {
+        
+    }
+
+
+}
